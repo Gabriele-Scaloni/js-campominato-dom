da30ab5b2cfdbee015ededf1cb7f3d9cb87a1803
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-03 15:51:50 +0100

    aggiunto numero casuale

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let domandaIniziale = prompt("scegli la difficltà tra 1, 2 o 3");
 
 let levels = 100; //di default, in base a quante volte divido il contenitore
 
-const numArr=[];
-
 
 
 //selezione livelli
@@ -23,23 +21,36 @@ if (domandaIniziale == 1) {
     levels = 49;
 } else if (domandaIniziale > 3 ||domandaIniziale < 1) {
     alert("riprova, i livelli sono da 1 a 3 ")
-    levels = 0;
+   
 }
 
-//genero numeri casuali da 1 a 100 * 16 volte e li metti in numArr
+/*ciclo while per generare 16 numeri  
+const bombe=[];
+while(bombs.length < 16){
+    const max = 49;
+    const min = 1;
+
+    const number=Math.floor(Math.random()*(max-min)+min);
+} */
+
+ //genero numeri casuali da 1 a 100 * 16 volte e li metti in numArr
+
+const numArr=[]; 
 for(var i=0;i<16;i++){
-var randomNumber = Math.floor(Math.random()*levels)+1;// il n random lo genero in base al numero di quadrati massimi del livello
-var nuovo = true;
-for(var j=0; j<i; j++){
-if(numArr[j]==randomNumber)nuovo=false;
-}
-if(nuovo){
-    numArr[i]=randomNumber;
-}else{
-i--;
-}
-} console.log(numArr);
+    var randomNumber = Math.floor(Math.random()*levels)+1;// il n random lo genero in base al numero di quadrati massimi del livello
+    var nuovo = true;
+    for(var j=0; j<i; j++){
+        if(numArr[j]==randomNumber)nuovo=false;
+        }
+        if(nuovo){
+            numArr[i]=randomNumber;
+        }else{
+        i--;
+    }
+} console.log(numArr);  
  
+
+
 const contenitore = document.getElementById("cont");
 
 for (let i = 0; i < levels; i++) {
@@ -50,18 +61,17 @@ for (let i = 0; i < levels; i++) {
     function(){ 
         newElement.classList.add("clickOnce");
         // numeri all'interno dei quadrati
-       newElement.append(i+1);
-       //newElement.append. classe per la bomba
+        newElement.append(i+1);
 
     });
-
+    
     contenitore.appendChild(newElement);
     console.log(contenitore);
-    
 }
+
 
 function newSquare (elemento, quadratoNuovo) {
     let node = document.createElement(elemento);
     node.classList.add(quadratoNuovo);
     return node
-} 
+}  
